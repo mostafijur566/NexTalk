2e59f15a49b8f59c91575b2a0fac5eb6212440d8
Mostafijur Rahman <mostafijur1812@gmail.com>
2026-05-19 02:25:06 +0600

    Add new cache and build configuration files for static web assets
    - Created rjsmcshtml.dswa.cache.json for caching Razor views.
    - Created rjsmrazor.dswa.cache.json for caching Razor components.
    - Created rpswa.dswa.cache.json for caching Razor pages.
    - Added staticwebassets.build.endpoints.json to define build endpoints.
    - Added staticwebassets.build.json for static web asset build configuration.
    - Introduced staticwebassets.build.json.cache for caching build hashes.
    - Added swae.build.ex.cache for additional build caching.

--- app/obj/Debug/net10.0/app.AssemblyInfo.cs
+++ app/obj/Debug/net10.0/app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4d49cc6a1cb38baef606eed81495c4392466296")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27ee6e7933d0ece95399cfa4b86245cdd212a8f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
